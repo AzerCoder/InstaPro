14cb33d948582427ac5da1b8e5e747768e54939c
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-01-29 16:34:43 +0500

    Initial Commit

---
+++ InstaPro/InstaProApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaProApp.swift
+//  InstaPro
+//
+//  Created by A'zamjon Abdumuxtorov on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
